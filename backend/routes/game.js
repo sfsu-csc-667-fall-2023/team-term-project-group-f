822304b718a0e822bb66635319d12d896c996b37
Scott McMann <57707915+SMcMann@users.noreply.github.com>
2023-12-08 22:30:33 -0800

    adding game creation route

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const { create } = require("../db/games");
 const router = express.Router();
 
 router.get("/:id", (request, response) => {
   const { id } = request.params;
 
   response.render("game", { id });
+});
+
+router.get("/all", (request, response) => {
+  const { id } = request.params;
+
+  response.render("game", { id });
+});
+
+router.post("/new_game", (request, response) => {
+  const { maxPlayers, creatorID, password } = request.params; // TODO implement passwords on games
+
+  // make a new game and set max players
+  // create(42);
+  // set the status to private and add a password if one is set
+
+  response.render("game", { id });
+});
+
+router.get("/debug", (request, response) => {
+  console.log("debug!!!");
 });
 
 module.exports = router;
--- backend/routes/global_lobby.js
+++ backend/routes/global_lobby.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_request, response) => {
+  console.log("GET global_lobby");
   response.render("global_lobby");
 });
 
