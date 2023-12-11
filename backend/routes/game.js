b7af8079a51b115a70c3514afd02ea43dde43940
Hoang Anh Tran <htran31@sfsu.edu>
2023-12-11 04:01:08 -0800

    updated login/signup db working

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -12,11 +12,29 @@ router.get("/:id", (request, response) => {
   response.render("game", { id: gameId });
 });
 
-router.post("/waiting_room", async (request, response) => {
-  const userId = request.session.userId; // Replace with actual user ID retrieval logic
-  const { gameId } = request.body;
+// router.post("/waiting_room", async (request, response) => {
+//   const userId = request.session.userId; // Replace with actual user ID retrieval logic
+//   const { gameId } = request.body;
 
-  console.log(request.session);
+//   console.log(request.session);
+
+//   try {
+//     await Games.addUser(userId, gameId);
+
+//     // Redirect to the game page or waiting lobby
+//     response.redirect(`/game/${gameId}`);
+//   } catch (error) {
+//     console.error("Error joining game:", error);
+//     response.status(500).send("Error joining game");
+//   }
+// });
+
+router.post("/waiting_room", async (request, response) => {
+  console.log("POST waiting_room triggered");
+  // const userId = request.session.user; // Replace with actual user ID retrieval logic
+  const { gameId, userId } = request.body;
+
+  console.log(request.body);
 
   try {
     await Games.addUser(userId, gameId);
@@ -26,6 +44,20 @@ router.post("/waiting_room", async (request, response) => {
   } catch (error) {
     console.error("Error joining game:", error);
     response.status(500).send("Error joining game");
+  }
+});
+
+// initialize game
+router.post("/:id/initialize", async (request, response) => {
+  const gameId = request.params.id;
+
+  try {
+    const gameState = await Games.initialize(gameId);
+
+    response.redirect(`/game/${gameId}`);
+  } catch (error) {
+    console.error("Error initializing game:", error);
+    response.status(500).send("Error initializing game");
   }
 });
 
--- backend/routes/log_in.js
+++ backend/routes/log_in.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_request, response) => {
-  response.render("landing");
+  response.render("log_in");
 });
 
 module.exports = router;
