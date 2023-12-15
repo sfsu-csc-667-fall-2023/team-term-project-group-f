5757b9fdecf2f8f75760e48c7cfd19f1c50a247b
Scott McMann <57707915+SMcMann@users.noreply.github.com>
2023-12-15 11:55:19 -0800

    fix

--- backend/db/games/get-last-card.js
+++ backend/db/games/get-last-card.js
@@ -2,11 +2,11 @@ const database = require("../connection");
 const { connection: db } = database;
 
 const GET_LAST_PLAYED_CARD = `
-  SELECT cards.* FROM game_cards
-  JOIN cards ON game_cards.card_id = cards.id
-  WHERE game_cards.game_id = $1 AND game_cards.user_id != 0
-  ORDER BY game_cards.card_order DESC
-  LIMIT 1
+  SELECT GC.*
+  FROM Games G
+  JOIN Cards GC ON G.lastPlayed = GC.id
+  WHERE G.id = $1
+  LIMIT 1;
 `; // order by card to play last card
 
 const getLastCard = (gameId) => db.oneOrNone(GET_LAST_PLAYED_CARD, [gameId]);
